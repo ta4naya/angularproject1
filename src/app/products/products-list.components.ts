7af4ac24b7091cdd9fb465661e6f37de1e863520
Anthony Igbokwe <seriousta4naya@outlook.com>
2018-10-10 19:46:59 +0200

    Initial files

---
+++ src/app/products/products-list.components.ts
@@ -0,0 +1,14 @@
+
+import { Component} from '@angular/core';
+
+@Component({
+    selector
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+  })
+
+
+
+  export class ProductListComponent {
+    pageTitle = 'Product List';
+  }
